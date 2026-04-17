6afe73976082a611ee0690b008d592f488996cfd
whiteowly <jerryayaylew696@gmail.com>
2026-04-17 22:35:11 +0300

    run tests

--- repo/run_tests.sh
+++ repo/run_tests.sh
@@ -11,7 +11,7 @@ echo "[run_tests] Running API tests..."
 docker compose run --rm app sh -c "npm install && npm run test:api"
 
 echo "[run_tests] Running E2E tests..."
-docker compose run --rm app sh -c "npm install && npx playwright install chromium && npm run test:e2e"
+docker compose run --rm app sh -c "npm install && npm run test:e2e"
 
 echo "[run_tests] Running build..."
 docker compose run --rm app sh -c "npm install && npm run build"
